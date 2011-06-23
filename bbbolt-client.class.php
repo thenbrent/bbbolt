954453d1fa1175ecfe8b97888de9c58463d87a7f
Brent Shepherd <thenbrent@gmail.com>
2011-06-23 22:55:02 +1000

    Changing parameters for client.

--- bbbolt-client.class.php
+++ bbbolt-client.class.php
@@ -12,15 +12,21 @@ class bbBolt_Client {
 
 	public function __construct( $name, $args = array() ){
 
-		$this->name          = $name;
-		$this->internal_name = sanitize_key( strtolower( $name ) );
-		$this->bbbolt_url    = $args['site_url'] . 'bbbolt/';
+		$defaults = array(
+			'labels' => array( 'name' => ucfirst( $name ) )
+		);
+
+		$args = wp_parse_args($args, $defaults);
+
+		$this->name          = sanitize_key( $name );
+		$this->labels        = (object)$args['labels'];
+		$this->bbbolt_url    = $args['site_url'] . '?bbbolt';
 
 		bbBolt_Client_UI::singleton();
 	}
-	
+
 	public function get_name() {
-		return $this->name;
+		return $this->labels->name;
 	}
 
 	public function get_url() {
