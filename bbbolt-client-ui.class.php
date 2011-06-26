049c8c752b361947c53bbf02a34648196cb12ec7
Brent Shepherd <thenbrent@gmail.com>
2011-06-26 15:53:07 +1000

    Adding loading image to bbbolt frame. List of plugins is now a list no longer paragraphs.

--- bbbolt-client-ui.class.php
+++ bbbolt-client-ui.class.php
@@ -83,20 +83,20 @@ class bbBolt_Client_UI {
 	 **/
 	public function support_form(){
 		global $bbbolt_clients;
-
 		?>
 		<div id="bbb_support_form">
 		<?php if ( count( $bbbolt_clients ) > 1 ) : ?>
 			<?php $iframe_src = 'about:blank'; ?>
 			<p><?php _e( 'Thanks for your call, to help us direct your call, please select the plugin for which you want to make a support request.', 'bbbolt' ); ?></p>
+			<ul id="bbb_client_list">
 			<?php foreach( $bbbolt_clients as $client ) : ?>
-				<p><a href="<?php echo $client->get_url(); ?>" target="bbbolt_frame">
-					<?php echo $client->get_name(); ?>
-				</a></p>
+				<li><a href="<?php echo $client->get_url(); ?>" target="bbbolt_frame"><?php echo $client->get_name(); ?></a></li>
 			<?php endforeach; ?>
+			</ul>
 		<?php else : ?>
 		<?php $iframe_src = $bbbolt_clients[0]->get_url(); ?>
 		<?php endif; ?>
+			<img id="loading" src="<?php echo get_bbbolt_dir_url(); ?>/images/loader.gif">
 			<iframe id="bbbolt_frame" name="bbbolt_frame" src="<?php echo $iframe_src; ?>" width="100%" height="100%">
 				<p><?php _e( "Uh oh, your browser does not support iframes. Please upgrade to a modern browser.", "bbbolt") ?></p>
 			</iframe>
@@ -175,6 +175,25 @@ class bbBolt_Client_UI {
 			box-shadow:inset 2px 0px 5px #CCC;
 			z-index: 50;
 		}
+		
+		#bbbolt_frame.loading {
+			opacity:0.2;
+		}
+		
+		img#loading {
+			display: none;
+			position: absolute;
+			top: 30%;
+			left: 25%;
+			z-index: 999;
+		}
+
+		#bbb_client_list li {
+			display: inline;
+			list-style-type: none;
+			margin-right: 1.5em;
+			padding-left: 4px;
+		}
 
 		#power-bbbolt {
 			font-size: 0.8em;
@@ -193,10 +212,18 @@ class bbBolt_Client_UI {
 	public function print_scripts() { ?>
 		<script>
 		jQuery(document).ready(function($) {
+			// Sliding Support Form
 			$('#bbb_support_slider #bbb_support_toggle').click(function() {
 				var $righty = $('#bbb_support_slider');
 				$righty.animate({ right: parseInt($righty.css('right'),10) == 0 ? -$righty.outerWidth() : 0});
 				$('#bbb_support_toggle a').text() == '<' ? $('#bbb_support_toggle a').text('>') : $('#bbb_support_toggle a').text('<');
+				return false;
+			});
+
+			// Loading animation for iframe
+			$('#bbb_client_list a').click(function(){
+				$('#bbbolt_frame').addClass('loading');
+				$('#loading').show();
 			});
 		});
 		</script>
