837caae73232c06b7cdb3ef3b7fa24c709c4af48
Brent Shepherd <thenbrent@gmail.com>
2011-06-26 14:47:34 +1000

    Adding paypal credentials to test-server plugin.

--- test-server.php
+++ test-server.php
@@ -11,8 +11,17 @@ require_once( 'bbbolt.php' );
 
 function register_test_bbbolt_server(){
 
+	// IMPORTANT: For security, you should store your API credentials in your site's
+	// database or wp-config.php. Storing real credentials in a source code file like
+	// this is not a good idea.
+	$paypal_credentials = array(
+		'username'  => 'digita_1308916325_biz_api1.gmail.com',
+		'password'  => '1308916362',
+		'signature' => 'AFnwAcqRkyW0yPYgkjqTkIGqPbSfAyVFbnFAjXCRltVZFzlJyi2.HbxW'
+	);
+
 	if( function_exists( 'register_bbbolt_server' ) ){
-		$bbbolt_server = register_bbbolt_server( 'bbb-test-server' );
+		$bbbolt_server = register_bbbolt_server( 'bbb-test-server', $paypal_credentials );
 	}	
 }
 add_action( 'init', 'register_test_bbbolt_server' );
