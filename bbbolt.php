ccf27e3c4f822e44e6ee1be030cac164a309d206
Brent Shepherd <thenbrent@gmail.com>
2011-06-27 19:42:46 +1000

    Better header info. Encasing each function in logic to prevent "function exists" errors.

--- bbbolt.php
+++ bbbolt.php
@@ -1,25 +1,46 @@
 <?php
 /*
- bbBolt
- Super simple support for WordPress Plugin Developers. 
- Author: Brent Shepherd
- Author URI: http://find.brentshepherd.com/
- Version: pre-alpha
+bbBolt
+Subscription Driven Support for WordPress Plugin Developers.
+Author: Leonard's Ego
+Author URI: http://leonardsego.com/
+Version: alpha
+Copyright: Leonard's Ego Pty. Ltd.
+License: GPLv2 or later
 */
 
-if( ! defined( 'BBBOLT_PLUGIN_BASENAME' ) )
-	define( 'BBBOLT_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
 if( ! defined( 'BBBOLT_PLUGIN_DIR' ) )
 	define( 'BBBOLT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-if( ! defined( 'BBBOLT_PLUGIN_URL' ) )
-	define( 'BBBOLT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 
-require_once( BBBOLT_PLUGIN_DIR . 'bbbolt-server.class.php' );
-require_once( BBBOLT_PLUGIN_DIR . 'bbbolt-client.class.php' );
-require_once( BBBOLT_PLUGIN_DIR . 'bbbolt-client-ui.class.php' );
+if( ! class_exists( 'bbBolt_Server' ) )
+	require_once( BBBOLT_PLUGIN_DIR . 'bbbolt-server.class.php' );
+
+if( ! class_exists( 'bbBolt_Client' ) )
+	require_once( BBBOLT_PLUGIN_DIR . 'bbbolt-client.class.php' );
+
+if( ! class_exists( 'bbBolt_Client_UI' ) )
+	require_once( BBBOLT_PLUGIN_DIR . 'bbbolt-client-ui.class.php' );
 
 
+if( ! function_exists( 'register_bbbolt_server' ) ) :
 /**
  * Register a bbBolt Server for your site. Do not use before init or with init priority later than 10.
  *
@@ -37,8 +58,10 @@ function register_bbbolt_server( $name, $args = array() ){
 
 	$bbbolt_server = new $bbbolt_server_class( $name, $args );
 }
+endif;
 
 
+if( ! function_exists( 'register_bbbolt_client' ) ) :
 /**
  * Register a bbBolt client. Do not use before init.
  *
@@ -59,8 +82,10 @@ function register_bbbolt_client( $name, $args = array() ){
 
 	$bbbolt_clients[] = new $bbbolt_client_class( $name, $args );
 }
+endif;
 
 
+if( ! function_exists( 'get_bbbolt_dir_url' ) ) :
 /**
  * Returns the URL to the location of this file's parent folder.
  * 
@@ -72,3 +97,4 @@ function get_bbbolt_dir_url() {
 	$path_after_plugin_dir = explode( 'plugins', dirname( __FILE__ ) );
 	return plugins_url() . $path_after_plugin_dir[1];
 }
+endif;
