d8a40e793a66d21acdbedbfb5cc67da8c951e14d
Brent Shepherd <thenbrent@gmail.com>
2011-07-29 15:11:01 +1000

    Better description & warning in example server.

--- example-server.php
+++ example-server.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: bbBolt Example Server
-Description: A demonstration code for setting up a bbBolt Server.
+Description: Demonstration of a bbBolt Server running on the site where this plugin is activate.
 Author: Brent Shepherd
 Author URI: http://find.brentshepherd.com/
 Version: beta-1
@@ -14,9 +14,8 @@ require_once( 'bbbolt-server.class.php' );
 function eg_register_bbbolt_server(){
 
 	if( function_exists( 'register_bbbolt_server' ) ){
-		// IMPORTANT: For security, you should store your API credentials in your site's
-		// database or wp-config.php. Storing real credentials in a source code file like
-		// this is not a good idea.
+		// IMPORTANT: For security, you should store your API credentials in your site's database or wp-config.php.
+		// Storing real credentials in a source code file like this is not a good idea.
 		$paypal_credentials = array(
 			'username'  => 'digita_1308916325_biz_api1.gmail.com',
 			'password'  => '1308916362',
